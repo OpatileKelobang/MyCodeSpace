5af2a62f08b1029a360c5864f17166c58ddc73b7
Opatile <kelobang.opatile@gmail.com>
2019-03-21 10:39:42 +0200

    Slide activity completed

--- app/src/main/java/co/za/bogote/mycodespace/WelcomeActivity.java
+++ app/src/main/java/co/za/bogote/mycodespace/WelcomeActivity.java
@@ -1,19 +1,33 @@
 package co.za.bogote.mycodespace;
 
+import android.os.Build;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class WelcomeActivity extends AppCompatActivity {
 
     private ViewPager mPager;
     private int[] layouts = {R.layout.first_slide, R.layout.second_slide};
+    private MpagerAdapter mpagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
+        if(Build.VERSION.SDK_INT >= 19)
+        {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+        else
+            {
+                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            }
+
         mPager = (ViewPager) findViewById(R.id.viewPager);
+        mpagerAdapter =  new MpagerAdapter(layouts, this);
+        mPager.setAdapter(mpagerAdapter);
     }
 }
