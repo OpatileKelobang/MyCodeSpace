488737b007b8f9a549243dadd3e3251c3f918bec
Opatile <kelobang.opatile@gmail.com>
2019-03-23 16:58:00 +0200

    Slide activity completed with dots

--- app/src/main/java/co/za/bogote/mycodespace/WelcomeActivity.java
+++ app/src/main/java/co/za/bogote/mycodespace/WelcomeActivity.java
@@ -1,16 +1,24 @@
 package co.za.bogote.mycodespace;
 
 import android.os.Build;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
 
 public class WelcomeActivity extends AppCompatActivity {
 
     private ViewPager mPager;
     private int[] layouts = {R.layout.first_slide, R.layout.second_slide};
     private MpagerAdapter mpagerAdapter;
+
+    private LinearLayout Dots_Layout;
+    private ImageView[] dots;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +37,53 @@ public class WelcomeActivity extends AppCompatActivity {
         mPager = (ViewPager) findViewById(R.id.viewPager);
         mpagerAdapter =  new MpagerAdapter(layouts, this);
         mPager.setAdapter(mpagerAdapter);
+
+        Dots_Layout = (LinearLayout) findViewById(R.id.dotsLayout);
+        createDots(0);
+
+        mPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                createDots(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
+
+    }
+
+
+    private void createDots(int currentPosition)
+    {
+       if (Dots_Layout != null)
+           Dots_Layout.removeAllViews();
+
+       dots = new ImageView[layouts.length];
+
+       for(int i = 0; i < layouts.length; i++)
+       {
+           dots[i] = new ImageView(this);
+           if (i == currentPosition)
+           {
+               dots[i].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.active_dots));
+           }
+           else
+           {
+               dots[i].setImageDrawable(ContextCompat.getDrawable(this, R.drawable.default_dots));
+           }
+
+       LinearLayout.LayoutParams params =  new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+           params.setMargins(4, 0, 4, 0);
+
+       Dots_Layout.addView(dots[i], params);
+       }
     }
 }
