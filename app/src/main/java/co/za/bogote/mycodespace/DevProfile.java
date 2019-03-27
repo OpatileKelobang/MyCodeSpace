88a1aa3fe13acd912775ae97d655a904a5414948
Opatile <kelobang.opatile@gmail.com>
2019-03-28 01:50:02 +0200

    Actionbar added and working. Back Button is working on Dev Profile

--- app/src/main/java/co/za/bogote/mycodespace/DevProfile.java
+++ app/src/main/java/co/za/bogote/mycodespace/DevProfile.java
@@ -7,10 +7,18 @@
 
 package co.za.bogote.mycodespace;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.SupportMenuInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
 import android.widget.Toolbar;
+
+import static co.za.bogote.mycodespace.R.menu.navigation_items;
 
 public class DevProfile extends AppCompatActivity {
 
@@ -22,6 +30,19 @@ public class DevProfile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dev_profile);
 
+    }
+
+    // Add Item To Menu
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+
+        return true;
+    }
+
+    public void goToMainScreen(View view)
+    {
+        startActivity(new Intent(this, MenuActivity.class));
+        finish();
     }
 
 
