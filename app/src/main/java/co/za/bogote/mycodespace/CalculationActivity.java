68ed13fde635e7dd9bb7010dfbe76311053c44e7
Opatile <kelobang.opatile@gmail.com>
2019-03-28 01:03:58 +0200

    Actionbar added and working. First step to testing app menu by adding items to it

--- app/src/main/java/co/za/bogote/mycodespace/CalculationActivity.java
+++ app/src/main/java/co/za/bogote/mycodespace/CalculationActivity.java
@@ -16,5 +16,6 @@ public class CalculationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculation);
+
     }
 }
--- app/src/main/java/co/za/bogote/mycodespace/DevProfile.java
+++ app/src/main/java/co/za/bogote/mycodespace/DevProfile.java
@@ -7,14 +7,22 @@
 
 package co.za.bogote.mycodespace;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class DevProfile extends AppCompatActivity {
+
+    private Toolbar myToolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dev_profile);
+
     }
+
+
 }
